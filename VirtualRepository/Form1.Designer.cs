4a19a04d418779bc46b297801cb29b3bb32437fd
turkmuhendisi <1turkmuhendisi@gmail.com>
2023-09-05 11:11:27 +0300

    Project finished.

--- VirtualRepository/Form1.Designer.cs
+++ VirtualRepository/Form1.Designer.cs
@@ -28,27 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.pNameTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(139, 143);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.deleteBtn);
+            this.groupBox2.Controls.Add(this.updateBtn);
+            this.groupBox2.Controls.Add(this.addBtn);
+            this.groupBox2.Controls.Add(this.priceTextBox);
+            this.groupBox2.Controls.Add(this.quantityTextBox);
+            this.groupBox2.Controls.Add(this.pNameTextBox);
+            this.groupBox2.Controls.Add(this.idTextBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // deleteBtn
+            // 
+            resources.ApplyResources(this.deleteBtn, "deleteBtn");
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            this.deleteBtn.MouseHover += new System.EventHandler(this.deleteBtn_MouseHover);
+            // 
+            // updateBtn
+            // 
+            resources.ApplyResources(this.updateBtn, "updateBtn");
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            this.updateBtn.MouseHover += new System.EventHandler(this.updateBtn_MouseHover);
+            // 
+            // addBtn
+            // 
+            resources.ApplyResources(this.addBtn, "addBtn");
+            this.addBtn.Name = "addBtn";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            this.addBtn.MouseHover += new System.EventHandler(this.addBtn_MouseHover);
+            // 
+            // priceTextBox
+            // 
+            resources.ApplyResources(this.priceTextBox, "priceTextBox");
+            this.priceTextBox.Name = "priceTextBox";
+            // 
+            // quantityTextBox
+            // 
+            resources.ApplyResources(this.quantityTextBox, "quantityTextBox");
+            this.quantityTextBox.Name = "quantityTextBox";
+            // 
+            // pNameTextBox
+            // 
+            resources.ApplyResources(this.pNameTextBox, "pNameTextBox");
+            this.pNameTextBox.Name = "pNameTextBox";
+            // 
+            // idTextBox
+            // 
+            resources.ApplyResources(this.idTextBox, "idTextBox");
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.TextChanged += new System.EventHandler(this.idTextBox_TextChanged);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1039, 556);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,22 +165,17 @@
 
         private GroupBox groupBox1;
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn id;
-        private DataGridViewTextBoxColumn pName;
-        private DataGridViewTextBoxColumn quantity;
-        private DataGridViewTextBoxColumn price;
-        private DataGridViewTextBoxColumn date;
         private GroupBox groupBox2;
         private Label label4;
         private Label label3;
         private Label label2;
         private Label label1;
-        private Button button1;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private Button button3;
-        private Button button2;
+        private Button deleteBtn;
+        private Button updateBtn;
+        private Button addBtn;
+        private TextBox priceTextBox;
+        private TextBox quantityTextBox;
+        private TextBox pNameTextBox;
+        private TextBox idTextBox;
     }
 }
--- VirtualRepository/Form1.cs
+++ VirtualRepository/Form1.cs
@@ -1,20 +1,116 @@
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
 namespace VirtualRepository
+
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        DataSet ds;
+        SqlDataAdapter adapter;
+        SqlCommand cmd;
+
+        void gridFill()
+        {
+            conn = new SqlConnection("server=.; Initial Catalog=virtualrepodb;Integrated Security=SSPI");
+            adapter = new SqlDataAdapter("SELECT * FROM products", conn);
+            ds = new DataSet();
+            conn.Open();
+            adapter.Fill(ds, "products");
+            dataGridView1.DataSource = ds.Tables["products"];
+            conn.Close();
+        }
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
+            gridFill();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void addBtn_Click(object sender, EventArgs e)
         {
+            string formattedDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
+            cmd = new SqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "INSERT INTO products (p_name, quantity, price, date) VALUES (@p_name, @quantity, @price, @date)";
+            cmd.Parameters.AddWithValue("@p_name", pNameTextBox.Text);
+            cmd.Parameters.AddWithValue("@quantity", quantityTextBox.Text);
+            cmd.Parameters.AddWithValue("@price", priceTextBox.Text);
+            cmd.Parameters.AddWithValue("@date", formattedDate);
 
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gridFill();
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE products SET p_name = @p_name, quantity = @quantity, price = @price WHERE p_id = @p_id";
+            cmd.Parameters.AddWithValue("@p_name", pNameTextBox.Text);
+            cmd.Parameters.AddWithValue("@quantity", quantityTextBox.Text);
+            cmd.Parameters.AddWithValue("@price", priceTextBox.Text);
+            cmd.Parameters.AddWithValue("@p_id", idTextBox.Text);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gridFill();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "DELETE FROM products WHERE p_id = @p_id";
+            cmd.Parameters.AddWithValue("@p_id", idTextBox.Text);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gridFill();
+        }
+
+        private void updateBtn_MouseHover(object sender, EventArgs e)
+        {
+            idTextBox.Enabled = true;
+        }
+
+        private void addBtn_MouseHover(object sender, EventArgs e)
+        {
+            idTextBox.Enabled = false;
+        }
+
+        private void deleteBtn_MouseHover(object sender, EventArgs e)
+        {
+            idTextBox.Enabled = true;
+        }
+
+        private void idTextBox_TextChanged(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT p_name, quantity, price FROM products WHERE p_id = @p_id";
+            cmd.Parameters.AddWithValue("@p_id", idTextBox.Text);
+            SqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read()) 
+            {
+                pNameTextBox.Text = dataReader.GetValue(0).ToString();
+                quantityTextBox.Text = dataReader.GetValue(1).ToString();
+                priceTextBox.Text = dataReader.GetValue(2).ToString();
+            }
+
+            conn.Close();
         }
     }
 }
